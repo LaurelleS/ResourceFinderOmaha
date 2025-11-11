75c784d1e756c51c6872a34f5b64df92072b447a
LaurelleS <laurelle887@gmail.com>
2025-11-10 22:10:41 -0600

    login page no styling

--- ResourceFinderOmaha/rfo/urls.py
+++ ResourceFinderOmaha/rfo/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signin/', include('django.contrib.auth.urls')),
+    path('account/', include('django.contrib.auth.urls')), # include django views and routes (account/login etc..)
 ]
--- ResourceFinderOmaha/users/apps.py
+++ ResourceFinderOmaha/users/apps.py
@@ -1,6 +1,32 @@
 from django.apps import AppConfig
 
 
+
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
+
+    def ready(self):
+
+        from django.db.utils import OperationalError
+        from django.contrib.auth.models import Group, Permission
+        from django.contrib.contenttypes.models import ContentType
+        
+        try:
+            # create group for orgs and regular users
+            finders_group, _ = Group.objects.get_or_create(name='Finders')
+            posters_group, _ = Group.objects.get_or_create(name='Organizations')
+
+            # create permissions for Event
+            users_ct = ContentType.objects.get(app_label='events', model='event')
+            can_add_event = Permission.objects.get(codename='add_event', content_type=users_ct)
+            can_delete_event = Permission.objects.get(codename='delete_event', content_type=users_ct)
+            can_change_event = Permission.objects.get(codename='change_event', content_type=users_ct)
+            can_view_event = Permission.objects.get(codename='view_event', content_type=users_ct)
+            
+            # set permisions for groups
+            posters_group.permissions.add(can_add_event, can_delete_event, can_change_event, can_view_event)
+            finders_group.permissions.add(can_view_event)
+            
+        except OperationalError:
+            pass
